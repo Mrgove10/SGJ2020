0b58d35c919a432983264150570faa84fed4b7ed
Adrien RICHARD <richardadrien0@gmail.com>
2020-02-07 23:08:46 +0100

    mouvemnt v1

--- Assets/Scripts/Mouvement.cs
+++ Assets/Scripts/Mouvement.cs
@@ -1,46 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Mouvement : MonoBehaviour
 {
-
-    CharacterController characterController;
-
     public float speed = 6.0f;
-
-    private Vector3 moveDirection = Vector3.zero;
-    
-    void Start()
-        {
-            characterController = GetComponent<CharacterController>();
-        }
 
     // Update is called once per frame
     void Update()
     {
-        if (characterController.isGrounded)
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
-            characterController.Move(moveDirection * Time.deltaTime);
-        }
-
-        /*if (Input.GetAxis("Vertical") > 1) //Front
-        {
-            player.Move(new)
-        }
-
-        if (Input.GetAxis("Vertical") < 1) //Back
-        {
-        }
-
-        if (Input.GetAxis("Horizontal") > 1) //Right
-        {
-        }
-
-        if (Input.GetAxis("Horizontal") < 1) //Left
-        {
-        }*/
+        transform.Rotate(0.0f, 0.0f, -Input.GetAxis("Horizontal") * speed * Time.deltaTime, Space.World);
+        transform.Rotate(-Input.GetAxis("Vertical") * speed * Time.deltaTime, 0.0f, 0.0f, Space.World);
     }
 }
---
+++ Assets/Scripts/PlayerHover.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+
+public class PlayerHover : MonoBehaviour
+{
+    public GameObject player;
+    
+    
+}
