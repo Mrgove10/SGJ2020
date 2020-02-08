59f508bf2659b703055cc061f521f2072310af21
hugo <hugo.quitto@gmail.com>
2020-02-08 02:42:09 +0100

    Popup when clicking on the mineral

--- Assets/Scripts/CheckGoodColor.cs
+++ Assets/Scripts/CheckGoodColor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CheckGoodColor : MonoBehaviour
 {
@@ -8,8 +9,10 @@ public class CheckGoodColor : MonoBehaviour
     [Range(0, 0.2f)]
     public float tolerence;
     public int clickOffset;
+    public GameObject showMineralUI;
 
     private bool _isWin = false;
+    private GameObject _popUP;
     private void Start()
     {
     }
@@ -18,7 +21,14 @@ public class CheckGoodColor : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            if (_popUP != null)
+            {
+                Destroy(_popUP);
+            }
             Color[] colorsClicked = GetComponent<SpriteRenderer>().sprite.texture.GetPixels((int)Input.mousePosition.x, (int)Input.mousePosition.y, clickOffset, clickOffset);
+            Color colorClicked = GetComponent<SpriteRenderer>().sprite.texture.GetPixel((int)Input.mousePosition.x, (int)Input.mousePosition.y);
+            Vector3 positionClicked = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
+            var worldPos = Camera.main.GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
             //Debug.Log("color of the pixel clicked = " + GetComponent<SpriteRenderer>().sprite.texture.GetPixel((int)Input.mousePosition.x, (int)Input.mousePosition.y).grayscale);
             var maxGoodColor = GoodColor;
             var minGoodColor = GoodColor;
@@ -38,6 +48,13 @@ public class CheckGoodColor : MonoBehaviour
                     Debug.Log("GG");
                 }
             }
+            Debug.Log(positionClicked);
+            var popUPgameObject = Instantiate(showMineralUI, positionClicked, Quaternion.identity);
+            var popUPText = popUPgameObject.GetComponentInChildren<TMP_Text>();
+            popUPText.transform.position = positionClicked;
+            popUPText.text = Random.Range(0, 15).ToString();
+            _popUP = popUPgameObject;
+            //Instantiate(showMineralUI, positionClicked, Quaternion.identity);
             _isWin = false;
 
         }
