170d72e349f846bb411637f16e6c1a97bc20f242
hugo <hugo.quitto@gmail.com>
2020-02-08 05:37:55 +0100

    pour adrien

---
+++ Assets/Tests/jesaispascequejefais.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class jesaispascequejefais : MonoBehaviour
+{
+    public RectTransform prefab;
+
+    private RectTransform waypoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var canvas = GameObject.Find("Waypoints").transform;
+        waypoint = Instantiate(prefab, canvas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
