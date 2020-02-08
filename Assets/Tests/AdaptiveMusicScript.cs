92ca817cf9993a44096244bc11ef2dfc629de71b
Théo <theo_38180@live.fr>
2020-02-08 21:31:26 +0100

    Ajout des musiques

---
+++ Assets/Tests/AdaptiveMusicScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdaptiveMusicScript : MonoBehaviour
+{
+    public AudioSource melodiaBase;
+    public AudioSource melodiaV1;
+    public AudioSource melodiaV2;
+    public AudioSource melodiaV3;
+    public AudioSource melodiaV4;
+
+    public bool melodiaPlayV1;
+    public bool melodiaPlayV2;
+    public bool melodiaPlayV3;
+    public bool melodiaPlayV4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (melodiaPlayV1)
+        {
+            melodiaV1.volume = 0.5f;
+        }
+        else
+        {
+            melodiaV1.volume = 0;
+        }
+
+        if (melodiaPlayV2)
+        {
+            melodiaV2.volume = 0.5f;
+        }
+        else
+        {
+            melodiaV2.volume = 0;
+        }
+
+        if (melodiaPlayV3)
+        {
+            melodiaV3.volume = 0.5f;
+        }
+        else
+        {
+            melodiaV3.volume = 0;
+        }
+
+        if (melodiaPlayV4)
+        {
+            melodiaV4.volume = 0.5f;
+        }
+        else
+        {
+            melodiaV4.volume = 0;
+        }
+    }
+}
---
+++ Assets/Tests/MouvementMusicScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouvementMusicScript : MonoBehaviour
+{
+    public AudioSource audioMouvement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("right") || Input.GetKey("left") || Input.GetKey("z") || Input.GetKey("q") || Input.GetKey("s") || Input.GetKey("d"))
+        {
+            audioMouvement.volume = 0.3f;
+        }
+        else
+        {
+            audioMouvement.volume = 0;
+        }
+    }
+}
