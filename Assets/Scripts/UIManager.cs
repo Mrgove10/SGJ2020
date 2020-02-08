4528475cdee7fe3ecc1ca11e245df85a6eae5c98
Adrien RICHARD <richardadrien0@gmail.com>
2020-02-08 03:16:21 +0100

    mods

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public Manager manager;
+
+    [Header("objects")]
+    public GameObject MainUI;
     public TMP_Text timeText;
     public TMP_Text scoreText;
-    public Manager manager;
+
+    public GameObject BreakUI;
+    public GameObject SliceUI;
+    public GameObject ClickUI;
 
     private void Update()
     {
         timeText.text = manager.time.ToString();
         scoreText.text = manager.points.ToString();
+
+        switch (manager.currentState)
+        {
+            case States.Roam:
+                MainUI.SetActive(true);
+                BreakUI.SetActive(false);
+                SliceUI.SetActive(false);
+                ClickUI.SetActive(false);
+                break;
+            case States.Break:
+                MainUI.SetActive(false);
+                BreakUI.SetActive(true);
+                SliceUI.SetActive(false);
+                ClickUI.SetActive(false);
+                break;
+            case States.Slice:
+                MainUI.SetActive(false);
+                BreakUI.SetActive(false);
+                SliceUI.SetActive(true);
+                ClickUI.SetActive(false);
+                break;
+            case States.Identify:
+                MainUI.SetActive(false);
+                BreakUI.SetActive(false);
+                SliceUI.SetActive(false);
+                ClickUI.SetActive(true);
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
     }
 }
