bb74a66bb62d7fa18b3ecc768fdb8ff0f5ed125b
Adrien RICHARD <richardadrien0@gmail.com>
2020-02-08 03:38:35 +0100

    .

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -1,10 +1,9 @@
-using System;
 using UnityEngine;
-
 
 public class Interaction : MonoBehaviour
 {
     public Manager manager;
+    public GameObject rock;
     private void Update()
     {
         if (Input.GetKey(KeyCode.E))
@@ -12,5 +11,21 @@ public class Interaction : MonoBehaviour
             Debug.Log("interaction");
             manager.currentState = States.Break;
         }
+
+        if (manager.currentState == States.Break)
+        {
+            TurnObject();
+        }
+    }
+
+    void TurnObject()
+    {
+        float rotationSpeed = 15;
+
+        float xAxis = Input.GetAxis("Mouse X") * rotationSpeed;
+        float yAxis = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        rock.transform.Rotate(Vector3.up, -xAxis, Space.World);
+        rock.transform.Rotate(Vector3.right, yAxis, Space.World);
     }
 }
--- Assets/Scripts/Mouvement.cs
+++ Assets/Scripts/Mouvement.cs
@@ -4,10 +4,15 @@ public class Mouvement : MonoBehaviour
 {
     public float speed = 6.0f;
 
+    public Manager manager;
+
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0.0f, 0.0f, -Input.GetAxis("Horizontal") * speed * Time.deltaTime, Space.World);
-        transform.Rotate(-Input.GetAxis("Vertical") * speed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+        if (manager.currentState == States.Roam)
+        {
+            transform.Rotate(0.0f, 0.0f, -Input.GetAxis("Horizontal") * speed * Time.deltaTime, Space.World);
+            transform.Rotate(-Input.GetAxis("Vertical") * speed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+        }
     }
 }
