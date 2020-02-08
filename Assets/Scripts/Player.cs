c8f210e8fcc24b4c08faae17225099019b2ce763
hugo <hugo.quitto@gmail.com>
2020-02-08 21:07:08 +0100

    Choix de ramassage cailloux

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,30 +1,43 @@
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     public float clamp = 0.0025f;
     public Manager manager;
     public Animator Animator;
+    public TMP_Text interactText;
 
+    private bool _isInAreaOfObject = false;
+
+    private void Start()
+    {
+        interactText.gameObject.SetActive(false);
+    }
     void OnTriggerEnter(Collider col)
     {
+        _isInAreaOfObject = true;
+        interactText.gameObject.SetActive(true);
         Debug.Log(col);
-        if (col.gameObject.CompareTag("Pickable"))
+        /*if (col.gameObject.CompareTag("Pickable"))
         {
             manager.currentState = States.Break;
-        }
+        }*/
     }
 
     private void Update()
     {
         Animator.SetFloat("Blend X", Input.GetAxis("Horizontal"));
         Animator.SetFloat("Blend Y", Input.GetAxis("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space) && _isInAreaOfObject == true)
+        {
+            manager.currentState = States.Break;
+            _isInAreaOfObject = false;
+            interactText.gameObject.SetActive(false);
+        }
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log(collision.gameObject.name);
-    }
     private void FixedUpdate()
     {
         transform.position += Vector3.up * (Mathf.Cos(Time.time) * clamp) / 100; //float effect
