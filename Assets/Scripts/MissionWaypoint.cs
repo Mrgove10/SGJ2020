5f0e7f93ba43d67e5fa0c2a53b9ad9428ea7dede
hugo <hugo.quitto@gmail.com>
2020-02-08 16:09:35 +0100

    caillou element ok

--- Assets/Scripts/MissionWaypoint.cs
+++ Assets/Scripts/MissionWaypoint.cs
@@ -70,50 +70,16 @@ public class MissionWaypoint : MonoBehaviour
             // Update the marker's position
             imgs[i].GetComponent<Image>().transform.position = pos;
             // Change the meter text to the distance with the meter unit 'm'
-            imgs[i].GetComponentInChildren<TMP_Text>().text = ((int)Vector3.Distance(targets[i].position, transform.position)).ToString() + "m";
+            if(((int)Vector3.Distance(targets[i].position, transform.position)) < 20)
+            {
+                imgs[i].GetComponentInChildren<TMP_Text>().text = "";
+            }
+            else
+            {
+                imgs[i].GetComponentInChildren<TMP_Text>().text = ((int)Vector3.Distance(targets[i].position, transform.position)).ToString() + "m";
+            }
         }
 
-        /*foreach(Transform target in targets)
-        {
-            
-            // Giving limits to the icon so it sticks on the screen
-            // Below calculations witht the assumption that the icon anchor point is in the middle
-            // Minimum X position: half of the icon width
-            float minX = img.GetPixelAdjustedRect().width / 2;
-            // Maximum X position: screen width - half of the icon width
-            float maxX = Screen.width - minX;
-
-            // Minimum Y position: half of the height
-            float minY = img.GetPixelAdjustedRect().height / 2;
-            // Maximum Y position: screen height - half of the icon height
-            float maxY = Screen.height - minY;
-            // Temporary variable to store the converted position from 3D world point to 2D screen point
-            Vector2 pos = Camera.main.WorldToScreenPoint(target.position + offset);
-
-            // Check if the target is behind us, to only show the icon once the target is in front
-            if (Vector3.Dot((target.position - transform.position), transform.forward) < 0)
-            {
-                // Check if the target is on the left side of the screen
-                if (pos.x < Screen.width / 2)
-                {
-                    // Place it on the right (Since it's behind the player, it's the opposite)
-                    pos.x = maxX;
-                }
-                else
-                {
-                    // Place it on the left side
-                    pos.x = minX;
-                }
-            }
-
-            // Limit the X and Y positions
-            pos.x = Mathf.Clamp(pos.x, minX, maxX);
-            pos.y = Mathf.Clamp(pos.y, minY, maxY);
-
-            // Update the marker's position
-            img.transform.position = pos;
-            // Change the meter text to the distance with the meter unit 'm'
-            meter.text = ((int)Vector3.Distance(target.position, transform.position)).ToString() + "m";
-        }*/
+        
     }
 }
