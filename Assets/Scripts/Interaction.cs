dd98c7ebe3c73af42937f992e51cc8d10425e855
Adrien RICHARD <richardadrien0@gmail.com>
2020-02-08 05:04:50 +0100

    .

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -1,20 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class Interaction : MonoBehaviour
 {
     public Manager manager;
     public GameObject rock;
+
+    private Random r;
+
+    private void Start()
+    {
+        r = new Random();
+    }
+
     private void Update()
     {
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             Debug.Log("interaction");
-            manager.currentState = States.Break;
+            var t = rock.transform.GetChild(r.Next(0, rock.transform.childCount));
+            if (t.transform.name.Contains("polySurface"))
+            {
+                t.GetComponent<Rigidbody>().GetComponent<Rigidbody>().useGravity = true;
+                StartCoroutine(deletebj(t.gameObject));
+            }
+         
         }
 
         if (manager.currentState == States.Break)
         {
             TurnObject();
+        }
+
+        if (rock.transform.childCount == 2)
+        {
+            if (rock.transform.name.Contains("PO1") && rock.transform.name.Contains("PO2"))
+            {
+                manager.currentState = States.Slice;
+            }
+        }
+    }
+
+    IEnumerator deletebj(GameObject t)
+    {
+        if (t)
+        {
+            yield return new WaitForSeconds(0.5f);
+            Destroy(t.gameObject);
         }
     }
 
